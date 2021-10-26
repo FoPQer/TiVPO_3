f928279df4e5f8f788204ce8fdd20d62d906995e
FoPQer <iritskiy.sas@gmail.com>
2021-10-26 20:53:19 +0300

    Релиз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Creator creator = new Creator();
+        creator.adder("836");
+        Deleter del = new Deleter(creator.file);
+        del.delete();
     }
 }
