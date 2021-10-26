d6c38efd3a4d25773cecc73a94a7b763eda2dfe9
FoPQer <iritskiy.sas@gmail.com>
2021-10-26 20:03:22 +0300

    Создан первый тест

--- test/DeleterTest.java
+++ test/DeleterTest.java
@@ -2,11 +2,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.io.File;
+import java.io.IOException;
 
 public class DeleterTest {
     @Test
-    public void deleteTest(){
+    public void deleteTest() throws IOException {
         File file = new File("D://TiVPO_3//Main.txt");
-        Assertions.assertEquals(file.length(), 0);
+        Deleter del = new Deleter(file);
+        Assertions.assertTrue(del.delete());
     }
 }
